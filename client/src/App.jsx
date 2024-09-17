77478f22c393699343b99800edca61d9031f2f00
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2024-09-17 23:01:47 +0300

    navbar

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar/Navbar"
+
+function App() {
+
+  return (
+    <div className="bg-white dark:bg-gray-800 dark:text-white duration-200 overflow-hidden">
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,33 @@
+import { IoMdSearch } from "react-icons/io";
+import Darkmode from "./Darkmode";
+
+const Navbar = () => {
+  return (
+    <div className="bg-white dark:bg-gray-800 dark:text-white duration-200 relative z-40">
+        <div className="py-4">
+            <div className="container flex justify-between items-center">
+                {/* Logo section */}
+                <div className="items-center p-3">
+                    <a href="#" className="text-purple-800 dark:text-purple-200 font-semibold tracking-widest text-2xl uppercase sm:text-3xl">bu bilet</a>
+                </div>
+
+                {/* Navbar right section */}
+                <div className="flex justify-between items-center gap-4">
+                    {/* Search section */}
+                    <div className="relative group hidden sm:block">
+                        <input type="text" placeholder="Search" className="search-bar" />
+                        <IoMdSearch className="text-xl text-purple-800 group-hover:text-purple-800 dark:text-purple-200 absolute top-1/2 -translate-y-1/2 right-3 duration-200"/>
+                    </div>
+
+                    {/* Dark mode section */}
+                    <div>
+                        <Darkmode />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
